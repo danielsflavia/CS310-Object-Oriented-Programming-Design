e9b54700c235af424cfc3444cd73c6257bcac08a
Flavia Daniels <danielsflavia@gmail.com>
2025-09-19 15:47:27 -0400

    exercise2

---
+++ inClass/Exercise2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    const int TOTAL_PRIMES = 50;
+    const int NUM_FOR_LINE = 10;
+
+    int count =0;
+    int number = 2;
+
+    while(count <= TOTAL_PRIMES) {
+        bool prime = true;
+        for(int i = 2; i * i <= number; i++){
+            if(number % i==0) {
+                prime = false;
+                break;
+            }
+        }
+    
+
+    if(prime) {
+        cout << setw(5) << number;
+        count++;
+    if (count % NUM_FOR_LINE == 0) {
+                cout << endl;  
+            }
+    }
+    number++;
+    }   
+
+    return 0;
+
+}
